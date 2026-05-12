c9701afab20f49b34325b5c8c5ae60656e44d971
Anja <anja.koprivica@dizzion.com>
2026-05-12 18:10:54 +0200

    Update provider versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,15 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.2"
     }
   }
 }
-
-#Napiši Terraform za:
-#1. `aws_iam_openid_connect_provider` za GitHub
-#2. `aws_iam_role` sa trust policy koji ograničava na `repo:koprivicaa/gha-playground:ref:refs/heads/main`
-#3. Policy koja dozvoljava samo: `sts:GetCallerIdentity`, `s3:ListAllMyBuckets`, 's3:GetObject' i 's3:PutObject' na bucketu `gha-playground-deployments`
-#4. Output role ARN
-
-#Apply, zabeleži ARN.
